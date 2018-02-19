0d94579d52031528cd1eeda5deefcc960f02ed07
Joaquin Vicente Alonso Saiz <joaalsai@ieslavereda.es>
2018-02-19 18:59:59 +0100

    Prueba

--- Semana1/ejercicio1.js
+++ Semana1/ejercicio1.js
@@ -65,3 +65,16 @@ vectorFilter=vectorMap.filter(function(elemento){
 });
 
 console.log(vectorFilter);
+
+// Ejercicio 4
+// Crea una función que reciba 3 parámetros (nombre de producto, precio e impuesto en porcentaje sobre 100). 
+
+// Dicha función hará lo siguiente:
+
+// Los parámetros deberán tener un valor por defecto por si no los recibe que deben ser: "Producto genérico", 100 y 21.
+
+// Convierte el nombre de producto a string (por si acaso) y los otros 2 a número. Si el precio o el impuesto no son numéros válidos muestra un error. 
+// Si son válidos, muestra por consola el nombre del producto y el precio final contando impuestos.
+
+// Llama a la función varias veces, omitiendo parámetros, con todos ellos, y pasándo algún valor no númerico en el precio o impuesto.
+
