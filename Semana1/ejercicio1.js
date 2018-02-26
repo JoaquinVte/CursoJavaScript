15614bcee9463abb4fe5f8f8947504584312eec4
Joaquin Vicente Alonso Saiz <joaalsai@ieslavereda.es>
2018-02-26 09:11:17 +0100

    Update ejercicio1.js
    Adapatación a curso 2017-2018

--- Semana1/ejercicio1.js
+++ Semana1/ejercicio1.js
@@ -1,13 +1,14 @@
-//  Ejercicio 1
-// Crea una función que reciba 2 cadenas por parámetro. Dicha función imprimirá por consola qué cadena tiene mayor longitud. 
-// Si el tipo de algún parámetro no es string (typeof param !== "string"), debes imprimir un error.
-
-// Llama a la función 3 veces con diferentes parámetros. En una de esas llamadas pásale por parámetro un valor que no sea string.
 'use strict';
 
-console.log("--------------------------");
-console.log("Ejercicio 1");
-console.log("--------------------------");
+/**
+ * Apartado 1
+ * Crea una función que reciba 2 cadenas por parámetro. Dicha función imprimirá por consola qué cadena
+ * tiene mayor longitud. Si el tipo de algún parámetro no es string (typeof param !== "string"),
+ * debes imprimir un error.
+ * Llama a la función 3 veces con diferentes parámetros. En una de esas llamadas pásale por parámetro un valor que no sea string.
+ */
+
+console.log('--------------- APARTADO 1 -----------------');
 
 function cadenaMayor(cadena1,cadena2){
 	if (typeof cadena1 !== "string" || typeof cadena2 !== "string") {
@@ -30,15 +31,14 @@ console.log(cadenaMayor("Hola","Adios"));
 console.log(cadenaMayor("En un lugar de la...","... Mancha"));
 console.log(cadenaMayor("Hola",3));
 
+/**
+ * Apartado 2
+ * Crea un array con diferentes tipos de valores (números, strings, booleanos). A partir de dicho array,
+ * recórrelo y genera otro array con dichos valores convertidos todos a número. Los que no hayan podido
+ * ser convertidos (NaN) no deberían estar en el array final. Imprime dicho array resultante por consola.
+ */
 
-// Ejercicio 2
-// Crea un array con diferentes tipos de valores (números, strings, booleanos). 
-// A partir de dicho array recórrelo y genera otro array con dichos valores convertidos todos a número. 
-// Los que no hayan podido ser convertidos (NaN) no deberían estar en el array final. Imprime dicho array resultante por consola.
-
-console.log("--------------------------");
-console.log("Ejercicio 2");
-console.log("--------------------------");
+console.log('--------------- APARTADO 2 -----------------');
 
 var vector = new Array(1,2,3,"Cuatro",true,6,7,false,"nueve");
 var vectorNumeros = new Array();
@@ -51,16 +51,15 @@ for (var idx in vector){
 
 console.log(vectorNumeros);
 
+/**
+ * Apartado 3
+ * Haz lo mismo que en el apartado anterior pero usando los métodos nuevos de EcmaScript 5 (map, filter, ...)
+ * Pista: Deberías primero generar un array con todos los valores convertidos a número (map), y a partir de
+ * este generar otro array filtrando los que no son NaN (filter). Con el array resultante, utiliza el método
+ * reduce para sumarlos.
+ */
 
-// Ejercicio 3
-// Haz lo mismo que en el apartado anterior pero usando los métodos nuevos de EcmaScript 5 (map, filter, ...)
-
-// Pista: Deberías primero generar un array con todos los valores convertidos a número (map), 
-// y a partir de este generar otro array filtrando los que no son NaN (filter).
-
-console.log("--------------------------");
-console.log("Ejercicio 3");
-console.log("--------------------------");
+console.log('--------------- APARTADO 3 -----------------');
 
 console.log(vector.map(function(elemento){
 	return Number(elemento);
@@ -68,22 +67,17 @@ console.log(vector.map(function(elemento){
 	return !isNaN(elemento);
 }));
 
+/**
+ * Apartado 4
+ * Crea una función que reciba 3 parámetros (nombre de producto, precio e impuesto en porcentaje sobre 100).
+ * Dicha función hará lo siguiente:
+ * - Los parámetros deberán tener un valor por defecto por si no los recibe que deben ser: "Producto genérico", 100 y 21.
+ * - Convierte el nombre de producto a string (por si acaso) y los otros 2 a número. Si el precio o el impuesto no son
+ *   numéros válidos muestra un error. Si son válidos, muestra por consola el nombre del producto y el precio final contando impuestos.
+ * - Llama a la función varias veces, omitiendo parámetros, con todos ellos, y pasándo algún valor no númerico en el precio o impuesto.
+ */
 
-// Ejercicio 4
-// Crea una función que reciba 3 parámetros (nombre de producto, precio e impuesto en porcentaje sobre 100). 
-
-// Dicha función hará lo siguiente:
-
-// Los parámetros deberán tener un valor por defecto por si no los recibe que deben ser: "Producto genérico", 100 y 21.
-
-// Convierte el nombre de producto a string (por si acaso) y los otros 2 a número. Si el precio o el impuesto no son numéros válidos muestra un error. 
-// Si son válidos, muestra por consola el nombre del producto y el precio final contando impuestos.
-
-// Llama a la función varias veces, omitiendo parámetros, con todos ellos, y pasándo algún valor no númerico en el precio o impuesto.
-
-console.log("--------------------------");
-console.log("Ejercicio 4");
-console.log("--------------------------");
+console.log('--------------- APARTADO 4 -----------------');
 
 function  precioTotal (nombre, precio, impuesto){
 	var name = nombre || "Producto genérico";
@@ -98,29 +92,18 @@ function  precioTotal (nombre, precio, impuesto){
 
 }
 
-console.log(precioTotal("coche",15000,21));
-console.log(precioTotal(null,15000,21));
-console.log(precioTotal("coche",15000,"casa"));
-console.log(precioTotal());
 
+/**
+ * Apartado 5
+ * Realiza los siguientes pasos (muestra por consola el resultado después de aplicar cada uno):
+ * - Crea un array con 4 elementos
+ * - Concatena 2 elementos más al final y 2 al principio
+ * - Elimina las posiciones de la 3 a la 5 (incluida)
+ * - Inserta 2 elementos más entre el penúltimo y el último
+ * - Muestra el array del paso anterior, pero con los elementos separados por "==>"
+ */
 
-// Ejercicio 5
-// Realiza los siguientes pasos (muestra por consola el resultado después de aplicar cada uno):
-
-// Crea un array con 4 elementos
-
-// Concatena 2 elementos más al final y 2 al principio
-
-// Elimina las posiciones de la 3 a la 5 (incluida)
-
-// Inserta 2 elementos más entre el penúltimo y el último
-
-// Muestra el array del paso anterior, pero con los elementos separados por "==>"
-
-console.log("--------------------------");
-console.log("Ejercicio 5");
-console.log("--------------------------");
-
+console.log('--------------- APARTADO 5 -----------------');
 
 vector = new Array(1,2,3,4);
 console.log(vector);
@@ -134,3 +117,35 @@ console.log(vector);
 
 vector.splice(vector.length-1,0,"antepenúltimo","penúltimo");
 console.log(vector.join("==>"));
+
+/**
+ * Apartado 6
+ * Crea una función que reciba 2 parámetros. El primero será el nombre de una persona,
+ * y el segundo serán los trabajos que ha realizado esa persona (usa spread para agruparlos en un array).
+ * Posible llamada -> printTrabajos("Pepe", "Albañil", "Programador", "Buscador de tesoros")
+ * La función simplemente mostrará por consola el nombre y los trabajos recorriéndolos con un for..of
+ */
+
+
+
+console.log('--------------- APARTADO 6 -----------------');
+
+/**
+ * Apartado 7
+ * A partir del siguiente array que contiene productos con mensajes sobre los mismos,
+ * filtra los mensajes que empiecen por ERROR (usa el método startsWith).
+ * Después recórrelos y mételos en un objeto Map cuya clave sea el nombre del producto
+ * y cuyo valor sea un array con los mensajes de error asociados al producto.
+ * Recorre el objeto Map mostrando, para cada producto, que errores tiene asociados.
+ */
+
+console.log('--------------- APARTADO 7 -----------------');
+
+let mensajes = [
+    ['Silla', 'ERROR: Stock no coincide'],
+    ['Mesa', 'Pedido de 2 unidades'],
+    ['Silla', 'ERROR: El precio no puede ser menor a 1'],
+    ['Mesa', 'ERROR: No se pueden enviar 0 unidades'],
+    ['Cama', 'ERROR: El fabricante no tiene ese modelo'],
+    ['Silla', 'Se ha borrado el producto de la base de datos']
+];
