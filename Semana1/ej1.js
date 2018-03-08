687aef0c90acb25692bb336f15fe3d134d40b39a
Joaquin Vicente Alonso Saiz <joaalsai@ieslavereda.es>
2018-03-08 10:38:47 +0100

    Correccion de errores

--- Semana1/ej1.js
+++ Semana1/ej1.js
@@ -88,7 +88,8 @@ function  precioTotal (nombre, precio, impuesto){
 		return "Error. El precio o impuesto no es un valor numerico.";
 	} else {
 		return "El coste de " + name + ", es de: " + (cost+(cost*tax/100)) +"€";
-	}Commits
+	}
+
 }
 
 console.log(precioTotal());
@@ -97,19 +98,7 @@ console.log(precioTotal("Coche","dosmil",));
 
 /**
  * Apartado 5
- * Realiza los siguientes pasos (muestra por
-'use strict';var persona = {    nombre: "Peter",    edad: 41,    trabajos: [ // trabajos es un array de objetos JSON        {            descripción: "Payaso triste",            duración: "2003-2005"        },        {            descripción: "Sexador de pollos",            duración: "2005-2015"
-￼
-PROBLEMAS
-3
-SALIDA
-CONSOLA DE DEPURACIÓN
-TERMINAL
-￼
-git rev-parse --symbolic-full-name --abbrev-ref master@{u}git rev-list --left-right master...origin/mastergit for-each-ref --format %(refname) %(objectname) --sort -committerdategit remote --verbosegit status -z -ugit symbolic-ref --short HEADgit rev-parse mastergit rev-parse --symbolic-full-name --abbrev-ref master@{u}git rev-list --left-right master...origin/mastergit for-each-ref --format %(refname) %(objectname) --sort -committerdategit remote --verbosegit show :Semana2/pruebas.jsgit checkout -q -- /home/joaalsai/CursoJavaScript/Semana2/pruebas.jsgit status -z -ugit symbolic-ref --short HEADgit rev-parse mastergit rev-parse --symbolic-full-name --abbrev-ref master@{u}git rev-list --left-right master...origin/mastergit for-each-ref --format %(refname) %(objectname) --sort -committerdategit remote --verbosegit status -z -ugit symbolic-ref --short HEADgit rev-parse mastergit rev-parse --symbolic-full-name --abbrev-ref master@{u}git rev-list --left-right master...origin/mastergit for-each-ref --format %(refname) %(objectname) --sort -committerdategit remote --verbosegit show :Semana2/pruebas.jsgit status -z -ugit symbolic-ref --short HEADgit rev-parse mastergit rev-parse --symbolic-full-name --abbrev-ref master@{u}git rev-list --left-right master...origin/mastergit for-each-ref --format %(refname) %(objectname) --sort -committerdategit remote --verbose ￼
-Lín. 19, Col. 46Espacios: 4UTF-8LFJavaScript master30
-Descargar ahoraMás tardeNotas de la versión
-Información consola el resultado después de aplicar cada uno):
+ * Realiza los siguientes pasos (muestra por consola el resultado después de aplicar cada uno):
  * - Crea un array con 4 elementos
  * - Concatena 2 elementos más al final y 2 al principio
  * - Elimina las posiciones de la 3 a la 5 (incluida)
@@ -178,12 +167,12 @@ let mensajes = [
 
 var mensajesError=mensajes.filter(function(elemento){
 	if (elemento[1].startsWith("ERROR")) {
-		return elemento;
+		return elemento
 	}
 });
 
 /**
- * Genero un Map con los distintos mensajes para cada key, siendo la key el 
+ * Genero un Map con los distintos mensajes para cada key siendo la key el 
  * nombre del producto y su valor un vector con los distintos mensajes de error
  */
 
@@ -192,14 +181,13 @@ for(let error of mensajesError){
 	if(!erroresMap.has(error[0])){
 		erroresMap.set(error[0],new Array());
 	}
-		
+	erroresMap.get(error[0]).push(error[1]);	
 }
 
-/**master
+/**
  * Recorremos el Map para visualizar su contenido
  */
 
 for(let error of erroresMap) {
  console.log("Errores de " + error[0] + ": " + error[1].join(", "));
 }
-master
