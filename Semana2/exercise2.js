0c847735940fda6205947da627594da05c6d85d0
Joaquin Vicente Alonso Saiz <joaalsai@ieslavereda.es>
2018-03-08 12:27:11 +0100

    Ejer

--- Semana2/exercise2.js
+++ Semana2/exercise2.js
@@ -2,3 +2,23 @@
 
 // WRITE ONLY HERE
 
+let comprobarCampos = function(event){
+    let reg = /\w/
+    // if (reg.test(document.getElementById("nombre").textContent)) {
+    //     console.log("texto");
+    // }
+    let nombre = document.getElementById("name").textContent;
+    if ( nombre.length == 0 || reg.test(nombre)) {
+        console.log("Valor no valido");
+    } else {
+        console.log(nombre);
+    }
+    console.log("boton presionado");
+}
+
+let button_primary = document.getElementsByTagName("button")[0];
+button_primary.addEventListener('click',comprobarCampos);
+
+
+
+
