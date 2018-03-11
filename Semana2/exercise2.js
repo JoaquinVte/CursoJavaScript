2e98e1dcf89b1cdf014592e81694be9d66a568ba
Joaquin Vicente Alonso Saiz <joaalsai@ieslavereda.es>
2018-03-11 16:54:10 +0100

    Incluir funcion borrarCampos

--- Semana2/exercise2.js
+++ Semana2/exercise2.js
@@ -49,16 +49,15 @@ let comprobarImagen = function (event) {
         imagen.className = "form-control is-valid";
     }
 }
-
 let comprobarCampos = function (event) {
     valido = true;
     let contenedor = document.getElementById("eventsContainer");
 
-    comprobarNombre(this);
-    comprobarFecha(this);
-    comprobarDescripcion(this);
-    comprobarPrecio(this);
-    comprobarImagen(this);
+    // comprobarNombre(this);
+    // comprobarFecha(this);
+    // comprobarDescripcion(this);
+    // comprobarPrecio(this);
+    // comprobarImagen(this);
 
     if (valido) {
         console.log(document.querySelectorAll(".card-deck:last-of-type").length);
@@ -69,18 +68,18 @@ let comprobarCampos = function (event) {
             let nodoCardDeck = contenedor.children[0]
             nodoCardDeck.setAttribute("class", "card-deck");
             nodoCardDeck.appendChild(document.createElement("div"));
-            crearCard(nodoCardDeck.children[0]);
+            completarCard(nodoCardDeck.children[0]);
         } else {
             if (document.querySelectorAll(".card-deck:last-of-type")[0].childNodes.length == 1) {
                 let nodoCardDeck = document.querySelectorAll(".card-deck:last-of-type")[0];
                 nodoCardDeck.appendChild(document.createElement("div"));
-                crearCard(nodoCardDeck.children[1]);
+                completarCard(nodoCardDeck.children[1]);
             } else {
                 let nodoCardDeck = document.createElement("div")
                 nodoCardDeck.setAttribute("class", "card-deck");
                 contenedor.appendChild(nodoCardDeck);
                 nodoCardDeck.appendChild(document.createElement("div"));
-                crearCard(nodoCardDeck.children[0]);
+                completarCard(nodoCardDeck.children[0]);
             }
 
         }
@@ -114,19 +113,19 @@ newEvent.image.addEventListener('change', event => {
     });
 });
 
-let crearCard = function (card) {
+let completarCard = function (card) {
     card.setAttribute("class", "card");
 
     card.appendChild(document.createElement("img"));
     card.children[0].setAttribute("class", "card-img-top");
-    // card.children[0].setAttribute("src", document.getElementById("imgPreview").src);
+    card.children[0].setAttribute("src", document.getElementById("imgPreview").getAttribute("src"));
 
     card.appendChild(document.createElement("div"));
     let cardBody = card.children[1]
     cardBody.setAttribute("class", "card-body");
     cardBody.appendChild(document.createElement("h4"));
     cardBody.children[0].setAttribute("class", "card-title");
-    cardBody.children[0].textContent = document.getElementById("name").value;
+    // cardBody.children[0].textContent = document.getElementById("name").value;
     cardBody.appendChild(document.createElement("p"));
     cardBody.children[1].setAttribute("class", "card-text");
     // cardBody.children[1].textContent = document.getElementById("description").value;
@@ -140,5 +139,27 @@ let crearCard = function (card) {
     cardFooter.children[0].appendChild(document.createElement("span"));
     cardFooter.children[0].children[0].setAttribute("class", "float-right");
     // cardFooter.children[0].children[0].value = document.getElementById("price").value;
+
+    borrarCampos()
+}
+
+function borrarCampos() {
+  
+    document.getElementById("name").className="form-control";
+    document.getElementById("name").value="";
+
+    document.getElementById("date").className="form-control";
+    document.getElementById("date").value="";
+
+    document.getElementById("description").className="form-control";
+    document.getElementById("description").value="";
+
+    document.getElementById("price").className="form-control";
+    document.getElementById("price").value="";
+
+    document.getElementById("image").className="form-control";
+    document.getElementById("imgPreview").setAttribute("src","");
+    document.getElementById("image").type='';
+    document.getElementById("image").type='file';
 }
 
