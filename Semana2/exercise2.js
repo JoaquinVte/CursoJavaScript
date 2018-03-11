2d12380fad285604b6e533161ac22030777b1f9f
Joaquin Vicente Alonso Saiz <joaalsai@ieslavereda.es>
2018-03-11 16:16:14 +0100

    Avances en ejercicio

--- Semana2/exercise2.js
+++ Semana2/exercise2.js
@@ -4,8 +4,8 @@
 let valido;
 
 let comprobarNombre = function (event) {
-
-    let nombre = document.getElementById("name");
+    let nombre;
+    nombre = document.getElementById("name");
     if (nombre.value.length == 0 || !/([a-z]|[A-Z])(\w+)?/.test(nombre.value)) {
         nombre.className = "form-control is-invalid";
         valido = false;
@@ -13,7 +13,6 @@ let comprobarNombre = function (event) {
         nombre.className = "form-control is-valid";
     }
 }
-
 let comprobarFecha = function (event) {
     let fecha = document.getElementById("date");
     if (fecha.value.length == 0 || !/^(\d{2}|\d{4})\/\d{2}\/\d{2}$/.test(fecha.value)) {
@@ -23,7 +22,6 @@ let comprobarFecha = function (event) {
         fecha.className = "form-control is-valid";
     }
 }
-
 let comprobarDescripcion = function (event) {
     let descripcion = document.getElementById("description");
     if (descripcion.value.length == 0 || !/([a-z]|[A-Z])(\w+)?/.test(descripcion.value)) {
@@ -33,7 +31,6 @@ let comprobarDescripcion = function (event) {
         descripcion.className = "form-control is-valid";
     }
 }
-
 let comprobarPrecio = function (event) {
     let precio = document.getElementById("price");
     if (precio.value.length == 0 || !/10000(.[0]{1,2})|[0-9]{4}(.[0-9]{1,2})?/.test(precio.value)) {
@@ -43,59 +40,51 @@ let comprobarPrecio = function (event) {
         precio.className = "form-control is-valid";
     }
 }
-
 let comprobarImagen = function (event) {
     let imagen = document.getElementById("image");
     let imagenPreview = document.getElementById("imgPreview");
     if (imagenPreview.getAttribute("src") == "") {
-        imagen.className="form-control is-invalid";
+        imagen.className = "form-control is-invalid";
     } else {
-        imagen.className="form-control is-valid";
+        imagen.className = "form-control is-valid";
     }
 }
 
 let comprobarCampos = function (event) {
-    valido=true;
-    let Contenedor=document.getElementById("eventsContainer");
-    
-    // comprobarNombre(this);
-    // comprobarFecha(this);
-    // comprobarDescripcion(this);
-    // comprobarPrecio(this);
-    // comprobarImagen(this);
+    valido = true;
+    let contenedor = document.getElementById("eventsContainer");
 
-    // <!-- <div class="card-deck mb-2">
-    //     <div class="card">
-    //       <img class="card-img-top" src="image_base64">
-    //       <div class="card-body">
-    //         <h4 class="card-title">Nombre del evento</h4>
-    //         <p class="card-text">Descripción.</p>
-    //       </div>
-    //       <div class="card-footer">
-    //         <small class="text-muted">
-    // 		        dd/mm/yyyy
-    // 		        <span class="float-right">Precio €</span>
-    //           </small>
-    //       </div>
-    //     </div>
-    //   </div> -->
-
+    comprobarNombre(this);
+    comprobarFecha(this);
+    comprobarDescripcion(this);
+    comprobarPrecio(this);
+    comprobarImagen(this);
 
     if (valido) {
-        
-        Contenedor.appendChild(document.createElement("div"));                        
-        
-        // newCard.appendChild(document.createElement("img").setAttribute("class", "card-img-top").setAttribute("src", "image_base64"));
-        // newCard.appendChild(document.createElement("div").setAttribute("class", "card-body"));
-        // newCard.appendChild(document.createElement("div").setAttribute("class", "card-footer"));
-        // let elemento = newCard.childNodes[1];
-        // elemento.appendChild(document.createElement("h4").setAttribute("class", "card-title"));
-        // elemento.childNodes[0].innerText = nombre.value;
-        // elemento.appendChild(document.createElement("p").setAttribute("class", "card-text"));
-        // elemento.childNodes[1].innerText = descripcion.value;
-        // elemento = newCard.nextSibling;
-    }
+        console.log(document.querySelectorAll(".card-deck:last-of-type").length);
+        console.log(document.querySelectorAll(".card-deck:last-of-type").children);
+        console.log("-------------------------");
+        if (document.querySelectorAll(".card-deck:last-of-type").length == 0) {
+            contenedor.appendChild(document.createElement("div"));
+            let nodoCardDeck = contenedor.children[0]
+            nodoCardDeck.setAttribute("class", "card-deck");
+            nodoCardDeck.appendChild(document.createElement("div"));
+            crearCard(nodoCardDeck.children[0]);
+        } else {
+            if (document.querySelectorAll(".card-deck:last-of-type")[0].childNodes.length == 1) {
+                let nodoCardDeck = document.querySelectorAll(".card-deck:last-of-type")[0];
+                nodoCardDeck.appendChild(document.createElement("div"));
+                crearCard(nodoCardDeck.children[1]);
+            } else {
+                let nodoCardDeck = document.createElement("div")
+                nodoCardDeck.setAttribute("class", "card-deck");
+                contenedor.appendChild(nodoCardDeck);
+                nodoCardDeck.appendChild(document.createElement("div"));
+                crearCard(nodoCardDeck.children[0]);
+            }
 
+        }
+    }
 }
 
 let inputName = document.getElementById("name");
@@ -113,7 +102,6 @@ inputPrecio.addEventListener('change', comprobarPrecio);
 let inputImagen = document.getElementById("image");
 inputPrecio.addEventListener('change', comprobarImagen);
 
-
 let button_primary = document.getElementsByTagName("button")[0];
 button_primary.addEventListener('click', comprobarCampos);
 
@@ -126,4 +114,31 @@ newEvent.image.addEventListener('change', event => {
     });
 });
 
+let crearCard = function (card) {
+    card.setAttribute("class", "card");
+
+    card.appendChild(document.createElement("img"));
+    card.children[0].setAttribute("class", "card-img-top");
+    // card.children[0].setAttribute("src", document.getElementById("imgPreview").src);
+
+    card.appendChild(document.createElement("div"));
+    let cardBody = card.children[1]
+    cardBody.setAttribute("class", "card-body");
+    cardBody.appendChild(document.createElement("h4"));
+    cardBody.children[0].setAttribute("class", "card-title");
+    cardBody.children[0].textContent = document.getElementById("name").value;
+    cardBody.appendChild(document.createElement("p"));
+    cardBody.children[1].setAttribute("class", "card-text");
+    // cardBody.children[1].textContent = document.getElementById("description").value;
+
+    card.appendChild(document.createElement("div"));
+    let cardFooter = card.children[2]
+    cardFooter.setAttribute("class", "card-footer");
+    cardFooter.appendChild(document.createElement("small"));
+    cardFooter.children[0].setAttribute("class", "text-muted");
+    // cardFooter.children[0].value = document.getElementById("date").value;
+    cardFooter.children[0].appendChild(document.createElement("span"));
+    cardFooter.children[0].children[0].setAttribute("class", "float-right");
+    // cardFooter.children[0].children[0].value = document.getElementById("price").value;
+}
 
