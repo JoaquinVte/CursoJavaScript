1c0c3e17954777a97b53237e099ab7b2cf96b332
Joaquin Vicente Alonso Saiz <joaalsai@ieslavereda.es>
2018-03-25 13:34:40 +0200

    Correccion de busqueda y ordenacion conjunta

--- Bloque3/ejercicio-bloque3/js/index.js
+++ Bloque3/ejercicio-bloque3/js/index.js
@@ -2,20 +2,23 @@
 
 // Variable global que almacena los eventos en un vector.
 var eventosGlobal = [];
+var eventosGlobalAuxiliar;
 
 EventItem.getEvents().then(events => {
     eventosGlobal = events;
+    eventosGlobalAuxiliar=eventosGlobal;
+    console.log("Hola");
     showEvents(eventosGlobal);
     //console.log(eventosGlobal);
 });
 
 // Funcion que muestra los eventos de un array pasados por parametro
-function showEvents(events) {
+function showEvents(eventos) {
     let container = document.getElementById("eventsContainer");
     while (container.firstChild) {
         container.removeChild(container.firstChild);
     }
-    events.forEach(e => {
+    eventos.forEach(e => {
         if (document.querySelectorAll(".card-deck:last-of-type").length == 0) {
             container.appendChild(document.createElement("div"));
             let nodoCardDeck = container.children[0];
@@ -38,21 +41,22 @@ function showEvents(events) {
 // Ordenar por Precio
 let orderByPrice = document.getElementById("orderPrice");
 orderByPrice.addEventListener("click", e => {
-    eventosGlobal.sort((e1, e2) => e1.price - e2.price);
-    showEvents(eventosGlobal);
+    eventosGlobalAuxiliar.sort((e1, e2) => e1.price - e2.price);
+    showEvents(eventosGlobalAuxiliar);
 });
 
 // Ordenar por Fecha
 let orderByDate = document.getElementById("orderDate");
 orderByDate.addEventListener("click", e => {
-    eventosGlobal.sort((e1, e2) => e1.date - e2.date);
-    showEvents(eventosGlobal);
+    eventosGlobalAuxiliar.sort((e1, e2) => e1.date - e2.date);
+    showEvents(eventosGlobalAuxiliar);
 });
 
 // Busqueda en el array coincidencias en el nombre,descripcion,fecha o precio
 let search = document.getElementById("searchEvent");
 search.addEventListener("keyup", e => {
-    let reg = new RegExp(document.getElementById("searchEvent").value, "gi");
-    let aux = eventosGlobal;
-    showEvents(aux.filter(ev => reg.test(ev.toString())));
+    let reg = new RegExp(document.getElementById("searchEvent").value, "i");
+    eventosGlobalAuxiliar = eventosGlobal;
+    eventosGlobalAuxiliar=eventosGlobalAuxiliar.filter(ev => reg.test(ev.toString()));
+    showEvents(eventosGlobalAuxiliar);
 })
